864c974d00a83f3733a5f8595f4aa04888efb85c
Jose Aquino <joseaquino@logikode.tech>
2019-03-12 05:22:20 +0000

    Added answered feedback for question one

--- app/src/main/java/tech/logikode/wordlyquiz/MainActivity.java
+++ app/src/main/java/tech/logikode/wordlyquiz/MainActivity.java
@@ -1,7 +1,9 @@
 package tech.logikode.wordlyquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkQuestionOneAnswered(View v) {
+        View qOneIndicator = findViewById(R.id.qOneAnsweredIndicator);
+
+        CheckBox optOne = findViewById(R.id.qOneOptOne);
+        CheckBox optTwo = findViewById(R.id.qOneOptTwo);
+        CheckBox optThree = findViewById(R.id.qOneOptThree);
+        CheckBox optFour = findViewById(R.id.qOneOptFour);
+        CheckBox optFive = findViewById(R.id.qOneOptFive);
+        CheckBox optSix = findViewById(R.id.qOneOptSix);
+
+        if (optOne.isChecked() || optTwo.isChecked() || optThree.isChecked() || optFour.isChecked() || optFive.isChecked() || optSix.isChecked()) {
+            qOneIndicator.setBackgroundResource(R.drawable.answered_indicator);
+        } else {
+            qOneIndicator.setBackgroundResource(R.drawable.not_answered_indicator);
+        }
     }
 }
