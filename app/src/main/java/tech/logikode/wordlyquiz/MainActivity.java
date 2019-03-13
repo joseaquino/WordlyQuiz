b32d33e7175a20866d8e271d88c5cf25bc6a6ecc
Jose Aquino <joseaquino@logikode.tech>
2019-03-13 03:29:08 +0000

    Removed empty spaces to the MainActivity class

--- app/src/main/java/tech/logikode/wordlyquiz/MainActivity.java
+++ app/src/main/java/tech/logikode/wordlyquiz/MainActivity.java
@@ -73,12 +73,10 @@ public class MainActivity extends AppCompatActivity {
         textField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
             }
 
             @Override
